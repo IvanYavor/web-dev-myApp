26bc1f4a3f612b1858cc5bf87ceb2a92bc930a8c
Ivan Yavorsky <02ivan0971@gmail.com>
2020-12-01 18:53:23 +0200

    Remove comments

--- App.js
+++ App.js
@@ -10,12 +10,6 @@ import {
 } from "react-native";
 
 export default function App() {
-  // const [state, setState] = useState({
-  //   s: "Enter a movie...",
-  //   results: [],
-  //   selected: {},
-  // });
-
   const movieStr = `{"Search":[{"Title":"Long title that want to break your layout. Long title that want to break your layout. Long title that want to break your layout. Long title that want to break your layout","Year":"2020","imdbID":"noid","Type":"test","Poster":"Poster_01.jpg"},{"Title":"Star Wars: Episode IV - A New Hope Star Wars: Episode IV - A New Hope ","Year":"1977","imdbID":"tt0076759","Type":"movie","Poster":"Poster_01.jpg"},{"Title":"Star Wars: Episode V - The Empire Strikes Back","Year":"1980","imdbID":"tt0080684","Type":"movie","Poster":"Poster_02.jpg"},{"Title":"Star Wars: Episode VI - Return of the Jedi","Year":"1983","imdbID":"tt0086190","Type":"movie","Poster":"Poster_03.jpg"},{"Title":"Star Wars: Episode VII - The Force Awakens","Year":"","imdbID":"tt2488496","Type":"movie","Poster":""},{"Title":"Star Wars: Episode I - The Phantom Menace","Year":"1999","imdbID":"tt0120915","Type":"movie","Poster":"Poster_05.jpg"},{"Title":"Star Wars: Episode III - Revenge of the Sith","Year":"2005","imdbID":"tt0121766","Type":"movie","Poster":"Poster_06.jpg"},{"Title":"Star Wars: Episode II - Attack of the Clones","Year":"2002","imdbID":"tt0121765","Type":"movie","Poster":"Poster_07.jpg"},{"Title":"Star Trek","Year":"2009","imdbID":"tt0796366","Type":"movie","Poster":"Poster_08.jpg"},{"Title":"Star Wars: Episode VIII - The Last Jedi","Year":"2017","imdbID":"tt2527336","Type":"","Poster":""},{"Title":"Rogue One: A Star Wars Story","Year":"2016","imdbID":"tt3748528","Type":"movie","Poster":"Poster_10.jpg"}]}`;
   let results = JSON.parse(movieStr).Search;
   const images = {
@@ -30,34 +24,8 @@ export default function App() {
     "Poster_10.jpg": require("./static/Posters/Poster_10.jpg"),
   };
 
-  // const movieStr = require("./static/MoviesList.txt");
-  // const movieStr = `{"Search":[{"Title":"Long title that want to break your layout. Long title that want to break your layout. Long title that want to break your layout. Long title that want to break your layout","Year":"2020","imdbID":"noid","Type":"test","Poster":"Poster_01.jpg"},{"Title":"Star Wars: Episode IV - A New Hope Star Wars: Episode IV - A New Hope ","Year":"1977","imdbID":"tt0076759","Type":"movie","Poster":"Poster_01.jpg"},{"Title":"Star Wars: Episode V - The Empire Strikes Back","Year":"1980","imdbID":"tt0080684","Type":"movie","Poster":"Poster_02.jpg"},{"Title":"Star Wars: Episode VI - Return of the Jedi","Year":"1983","imdbID":"tt0086190","Type":"movie","Poster":"Poster_03.jpg"},{"Title":"Star Wars: Episode VII - The Force Awakens","Year":"","imdbID":"tt2488496","Type":"movie","Poster":""},{"Title":"Star Wars: Episode I - The Phantom Menace","Year":"1999","imdbID":"tt0120915","Type":"movie","Poster":"Poster_05.jpg"},{"Title":"Star Wars: Episode III - Revenge of the Sith","Year":"2005","imdbID":"tt0121766","Type":"movie","Poster":"Poster_06.jpg"},{"Title":"Star Wars: Episode II - Attack of the Clones","Year":"2002","imdbID":"tt0121765","Type":"movie","Poster":"Poster_07.jpg"},{"Title":"Star Trek","Year":"2009","imdbID":"tt0796366","Type":"movie","Poster":"Poster_08.jpg"},{"Title":"Star Wars: Episode VIII - The Last Jedi","Year":"2017","imdbID":"tt2527336","Type":"","Poster":""},{"Title":"Rogue One: A Star Wars Story","Year":"2016","imdbID":"tt3748528","Type":"movie","Poster":"Poster_10.jpg"}]}`;
-  // let results = JSON.parse(movieStr).Search;
-  // const images = {
-  //   "Poster_01.jpg": require("./static/Posters/Poster_01.jpg"),
-  //   "Poster_02.jpg": require("./static/Posters/Poster_02.jpg"),
-  //   "Poster_03.jpg": require("./static/Posters/Poster_03.jpg"),
-  //   "": "",
-  //   "Poster_05.jpg": require("./static/Posters/Poster_05.jpg"),
-  //   "Poster_06.jpg": require("./static/Posters/Poster_06.jpg"),
-  //   "Poster_07.jpg": require("./static/Posters/Poster_07.jpg"),
-  //   "Poster_08.jpg": require("./static/Posters/Poster_08.jpg"),
-  //   "Poster_10.jpg": require("./static/Posters/Poster_10.jpg"),
-  // };
-  // results.forEach((result) => {
-  //   result.src = require(`./static/Posters/${result.Poster}`);
-  // });
-
-  // const display = () => {
-  //   let results = JSON.parse(movieStr).Search;
-  //   setState((prevState) => {
-  //     return { ...prevState, results: results };
-  //   });
-  // };
-
   return (
     <View style={styles.container}>
-      {/* <Text style={styles.title}>Movies</Text> */}
       <ScrollView style={styles.results}>
         {results.map((result) => (
           <View key={result.imdbID} style={styles.result}>
@@ -67,7 +35,6 @@ export default function App() {
                 width: "100%",
                 height: 300,
               }}
-              // resizeMode="cover"
             ></Image>
             <Text style={styles.heading}>{result.Title}</Text>
             <Text style={styles.heading}>{result.imdbID}</Text>
